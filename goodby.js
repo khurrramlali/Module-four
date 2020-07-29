f252df5883530afa7e314eb5c7b16f727350a060
khurrramlali <62182850+khurrramlali@users.noreply.github.com>
2020-07-29 10:41:57 +0500

    Delete goodby.js

--- goodby.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    var speakWord = "Goodbye";
-    var byeSpeaker = function (name) {
-        console.log(speakWord + " " + name);
-    }
-
-    window.byeSpeaker = byeSpeaker;
-
-})(window);
